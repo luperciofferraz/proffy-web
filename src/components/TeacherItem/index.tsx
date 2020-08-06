1d63f9528ed22f13054d97a470d8f6f511c29aef
Lupércio Ferraz <lupercio.ferraz@gmail.com>
2020-08-06 17:53:36 -0300

    Ultimo commit 3.o dia NLW2

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,36 +1,56 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('connections', {
+
+            user_id: teacher.id,
+
+        });
+    }
 
     return (
         
         <article className="teacher-item">
         
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/5264225?s=460&u=bcb30a610e8d9b852599ea025770f4534e7b458d&v=4" alt="Lupércio F. Ferraz"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Lupércio Ferraz</strong>
-                    <span>Computação</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
         
-            <p>
-                Entusiasta das melhores práticas da programação.
-                <br/><br/>
-                Apaixonado por Java e React.
-            </p>
+            <p>{teacher.bio}</p>
         
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$ 80,00</strong>
+                    <strong>{teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} >
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         
         </article>
